859c552a620c8b9547477cce016af6ee8e7736b8
Vinicios Gabriel <vini.webdeveloper@gmail.com>
2022-06-30 20:59:53 -0300

    login controller and service

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,7 +1,22 @@
-const authService = require('./auth.service');
+const authService = require("./auth.service");
+const bcrypt = require("bcryptjs");
 
-const loginController = async(req, res) => {
-    res.send({ message: "login ok"});
-}
+const loginController = async (req, res) => {
+  const { email, password } = req.body;
 
+  const user = await authService.loginService(email);
+
+  if (!user) {
+    return res.status(400).send({ message: "invalid Email, try again!" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(400).send({ message: "invalid password, try again!" });
+  }
+
+  res.send(user);
+  
+};
 module.exports = { loginController };
