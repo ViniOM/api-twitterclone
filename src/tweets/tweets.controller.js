2af3590ac36b7fcc25c1570efc24fdfed93e0593
Vinicios Gabriel <vini.webdeveloper@gmail.com>
2022-07-03 08:09:01 -0300

    add find all tweets

---
+++ src/tweets/tweets.controller.js
@@ -0,0 +1,49 @@
+const tweetService = require("./tweets.service");
+
+const createTweetController = async (req, res) => {
+  try {
+    const { message } = req.body;
+
+    if (!message) {
+      res.status(400).send({
+        message: "Envie todos os dados necessário para a criação do tweet",
+      });
+    }
+
+    const { id } = await tweetService.createTweetService(message, req.userId);
+
+    return res.send({
+      message: "Tweet criado com sucesso!",
+      tweet: { id, message },
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+const findAllTweetsController = async (req, res) => {
+  try {
+    const tweets = await tweetService.findAllTweetsService();
+
+    if(tweets.length === 0) {
+      return res.status(400).send({ message: "Não existem tweets!"})
+    }
+    return res.send({
+      results: tweets.map((tweet) => ({
+        id: tweet._id,
+        message: tweet.message,
+        likes: tweet.likes.length,
+        comments: tweet.comments.length,
+        retweets: tweet.retweets.length,
+        name: tweet.user.name,
+        username: tweet.user.username,
+        avatar: tweet.user.avatar   
+      })),
+    });
+  }
+  catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}
+
+module.exports = {createTweetController, findAllTweetsController}
---
+++ src/tweets/tweets.service.js
@@ -0,0 +1,8 @@
+const Tweet = require("./Tweet");
+
+const createTweetService = (message, userId) =>
+  Tweet.create({ message, user: userId });
+
+const findAllTweetsService = () => Tweet.find().sort({ _id: -1 }).populate("user");
+
+module.exports = { createTweetService, findAllTweetsService };
